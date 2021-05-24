9c29f4efe96112a63b63f554bce3371f51329db4
tomasz <tomasz.suchodolski@vavatech.pl>
2021-05-24 23:11:11 +0200

    document roles and user list disabled for employees

--- frontend/src/view/logged/main/MainView.vue
+++ frontend/src/view/logged/main/MainView.vue
@@ -56,9 +56,7 @@ export default {
         { title: 'Dokumenty', address: '/app/documents', icon: 'description' },
         { title: 'Moje dokumenty', address: '/app/documents/own', icon: 'description' },
         { title: 'Zbiory', address: '/app/series', icon: 'folder_open' },
-        { title: 'Moje zbiory', address: '/app/series/own', icon: 'folder_open' },
-        { title: 'Użytkownicy', address: '/app/users', icon: 'person' },
-        { title: 'Role w dokumentach', address: '/app/document-roles', icon: 'portrait' }
+        { title: 'Moje zbiory', address: '/app/series/own', icon: 'folder_open' }
       ],
     }
   },
@@ -66,12 +64,21 @@ export default {
   created() {
     api.get(this, '/users/logged', null, successResponse => {
       this.user = successResponse;
+      if (this.adminLogged()) {
+        this.links.push(
+            { title: 'Użytkownicy', address: '/app/users', icon: 'person' },
+            { title: 'Role w dokumentach', address: '/app/document-roles', icon: 'portrait' })
+      }
     }, errorResponse => {
       console.log(errorResponse);
     });
   },
 
   methods: {
+    adminLogged() {
+      return this.user.systemRole === "Administrator";
+    },
+
     logout() {
       auth.logout(this);
     },
